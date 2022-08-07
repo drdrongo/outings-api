177a6c6bef933801addd82b67d23b67c4a1fb0a3
hayato clarke <hayatoclarke@gmail.com>
2022-08-07 23:37:31 +0900

    faker in prod

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,16 +1,43 @@
-# Be sure to restart your server when you modify this file.
+# config/initializers/cors.rb
+# frozen_string_literal: true
 
 # Avoid CORS issues when API is called from the frontend app.
 # Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
 
-# Read more: https://github.com/cyu/rack-cors
+# to_regexp = ->(string) { Regexp.new(string) }
+# hosts = [
+#   *ENV.fetch('ALLOWED_ORIGINS').split(',')
+#   # *ENV.fetch('ALLOWED_ORIGIN_REGEXPS').split(';').map(&to_regexp)
+# ]
+
+# Rails.application.config.middleware.insert_before 0, Rack::Cors do
+#   allow do
+#     origins(*hosts)
+
+#     resource '*',
+#              methods: %i[get post put patch delete options head],
+#              headers: :any
+#   end
+# end
+
+
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'http://localhost:3000'
     resource '*',
-      :headers => :any,
-      :methods => [:get, :post, :put, :delete]
+      headers: :any,
+      methods: [:get, :post, :put, :delete]
+    # resource '*',
+    #   headers: :any,
+    #   methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+
+  allow do
+    origins 'http://localhost:3000'
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :delete]
     # resource '*',
     #   headers: :any,
     #   methods: [:get, :post, :put, :patch, :delete, :options, :head]
